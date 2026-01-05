6613939b9a8a005a9f45283073256ccd524a08ad
Salman-sajib <sfsajib@gmail.com>
2026-01-05 14:43:17 +0600

    fake commit

--- components/architect/Experience/Education.jsx
+++ components/architect/Experience/Education.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+// comment
+
 export default function Education({ item }) {
   return (
-    <article className="w-full max-w-[500px] h-[230px] relative p-10 pl-15">
+    <article className="w-full max-w-[500px] h-full min-h-[230px] relative p-10 pl-15">
       <div className="space-y-3">
         <h4 className="text-2xl font-medium font-lora">{item.institution}</h4>
         <p className="text-lg text-text-secondary">{item.degree}</p>
