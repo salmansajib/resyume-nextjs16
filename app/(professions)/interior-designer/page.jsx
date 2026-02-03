25a44eb134b3325c2f035a34dbc33b607fe6dae4
Salman-sajib <sfsajib@gmail.com>
2026-02-03 18:00:58 +0600

    experience section added to the interior designer

--- app/(professions)/interior-designer/page.jsx
+++ app/(professions)/interior-designer/page.jsx
@@ -7,6 +7,7 @@ import PortfolioSection from "@/components/interior-designer/portfolio/Portfolio
 import HobbiesSection from "@/components/interior-designer/hobbies/HobbiesSection";
 import ReferralsSection from "@/components/interior-designer/referrals/ReferralsSection";
 import FooterSection from "@/components/interior-designer/footer/FooterSection";
+import ExperienceSection from "@/components/interior-designer/experience/ExperienceSection";
 
 export default function InteriorDesignerHome() {
   const data = interiorDesignerData;
@@ -19,6 +20,7 @@ export default function InteriorDesignerHome() {
       <HeaderSection data={data} />
       <main>
         <ServicesSection data={data} />
+        <ExperienceSection data={data} />
         <SkillsSection data={data} />
         <PortfolioSection data={data} />
         <HobbiesSection data={data} />
